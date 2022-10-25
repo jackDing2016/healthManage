8e877ac9805ef7d75c7b2d6ee39fac6a8da8dcb2
jack <jackding2015@gmail.com>
2022-10-25 06:28:16 -0400

    code commit

--- src/main/java/com/jack/healthManage/dto/SleepAddDTO.java
+++ src/main/java/com/jack/healthManage/dto/SleepAddDTO.java
@@ -1,5 +1,7 @@
 package com.jack.healthManage.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -7,8 +9,10 @@ import java.time.LocalDateTime;
 @Data
 public class SleepAddDTO implements java.io.Serializable {
 
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime startTime;
 
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime endTime;
 
     private Integer sleepQuality;
